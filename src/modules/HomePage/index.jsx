a39d084357e1f952ddbf8e68f4bb0bdcb5eab904
Korpan-Artem <arttodoc@gmail.com>
2022-11-30 17:26:22 +0200

    added nav

---
+++ src/modules/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header"
+import Search from "../../components/Search"
+
+function HomePage() {
+    return (
+        <>
+            <Header />
+            <Search/>
+        </>
+    );
+}
+
+export default HomePage;
